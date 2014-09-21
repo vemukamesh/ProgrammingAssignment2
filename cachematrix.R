dcd35d1197bacede05c27f293c26de1d5920a0b3
Kamesh Vemu <vemukamesh@gmail.com>
2014-09-21 10:57:17 -0400

    First Commit
    Changed files for programming assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##MakeCacheMatrix returns list of functions
+## Function          Purpose
+##________________________________
+## setM             Sets value of the matrix
+## getM             Gets value of the matrix
+## cacheInv         Gets cached value of matrix
+## getInv           Gets cached value of matrix  
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  cache <- NULL                               #Initially cached value is NULL
+  
+  setM <- function(y) {                       #store matrix
+    x <<- y    
+    cache <<- NULL                           #flush cache since new value "y"
+  }
+  
+  getM <- function() x                        #return stored matrix
 
+  cacheInv <- function(solve) {               #cache the inverse
+    cache <<- solve
+  }
+   
+  getInv <- function() cache                  #getcached value
+    
+  list(setM = setM, getM = getM, cacheInv = cacheInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Function gets the inverse of a matrix
+## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(y=matrix(), ...) {
+       
+        inverse <- y$getInv()
+        
+        if(!is.null(inverse)) {
+        message("have cached data")
+        return(inverse)
+        } else {
+        message("need to calculate inverse")
+        data <- y$getM()
+        inverse <- solve(data)
+        y$cacheInv(inverse)
+        return(inverse)
+        }
 }
